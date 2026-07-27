bebfc1730b3a676d31cdd2d401839298181b063f
Al Mele <amele@carolina.rr.com>
2026-07-27 13:28:10 -0400

    refactor herd animals

--- src/main/java/com/deathfrog/mctradepost/apiimp/initializer/ModBuildingsInitializer.java
+++ src/main/java/com/deathfrog/mctradepost/apiimp/initializer/ModBuildingsInitializer.java
@@ -134,6 +134,7 @@ public final class ModBuildingsInitializer
             petShopBuilder.setRegistryName(ResourceLocation.fromNamespaceAndPath(MCTradePostMod.MODID, ModBuildings.PETSHOP_ID));
             petShopBuilder.addBuildingModuleProducer(MCTPBuildingModules.ANIMALTRAINER_WORK);
             petShopBuilder.addBuildingModuleProducer(MCTPBuildingModules.PET_TRAINING);
+            petShopBuilder.addBuildingModuleProducer(MCTPBuildingModules.HERD_ANIMALS);
             petShopBuilder.addBuildingModuleProducer(MCTPBuildingModules.PET_ASSIGNMENT);
             petShopBuilder.addBuildingModuleProducer(BuildingModules.STATS_MODULE);
             ModBuildings.petshop = petShopBuilder.createBuildingEntry();
